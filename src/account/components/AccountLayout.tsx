edc9b5ba14dd8fb716ae2755a580ed9404b6ef54
Emir <emirhannaneli@gmail.com>
2025-11-20 03:44:16 +0300

    Refactor AccountLayout component to remove unused AccountSidebar import and simplify props structure.

--- src/account/components/AccountLayout.tsx
+++ src/account/components/AccountLayout.tsx
@@ -1,12 +1,11 @@
 import { type KcContext } from "../KcContext";
-import { AccountSidebar } from "./AccountSidebar";
 
 interface AccountLayoutProps {
     kcContext: KcContext;
     children: React.ReactNode;
 }
 
-export function AccountLayout({ kcContext, children }: AccountLayoutProps) {
+export function AccountLayout({ children }: AccountLayoutProps) {
     return (
         <div className="min-h-screen flex flex-col">
             <main className="flex-1 px-4 md:px-6 py-6 flex items-center justify-center w-full pt-24">
